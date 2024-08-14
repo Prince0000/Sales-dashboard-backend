b9cb7da0690b79cfbfe27681da6b23c6827785de
Prince Raj <39306689+Prince0000@users.noreply.github.com>
2024-08-15 02:07:56 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -3,9 +3,7 @@ const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
-app.use(cors({
-  origin: 'https://master--neon-creponne-400bfd.netlify.app'
-}));
+app.use(cors());
 
 app.use(express.json());
 
